2db7c5303fa0fde5bfb5e987411078708a4001c6
Bilgehan Varli <bilgehanvarli@gmail.com>
2014-11-17 08:49:58 +0200

    Optimize toString

--- HW-02/IyteMutableList.scala
+++ HW-02/IyteMutableList.scala
@@ -1,4 +1,5 @@
 import scala.collection.mutable.ArrayBuffer
+import java.lang.StringBuilder
 
 class IyteMutableList {
   var array = new ArrayBuffer[Int]()
@@ -8,15 +9,18 @@ class IyteMutableList {
   }
 
   override def toString = {
-    var str = array.applyOrElse(0, {_: Int => ""}).toString
-    // execute if size > 0
-    for (i <- 1 until array.length) {
-      str = str.concat("," +  array(i))
+    if(array.size == 0) ""
+    else {
+      val strBuf = new StringBuilder(array(0).toString)
+      for (i <- 1 until array.length) {
+        strBuf.append("," +  array(i))
+      }
+      strBuf.toString
     }
-    str
   }
 }
 
 object IyteMutableList extends IyteMutableList{
   def apply = new IyteMutableList()
 }
+
